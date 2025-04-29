6b2f69d7cb1f13a8c06ef71310dab58d5b7c9417
d0nil <moa_pochta_777@mail.ru>
2025-04-29 15:59:07 +0300

    refactor: add password validation

--- src/Application/SocialNetwork.Application/Validations/UserValidation.cs
+++ src/Application/SocialNetwork.Application/Validations/UserValidation.cs
@@ -4,6 +4,16 @@ public static class UserValidation
 {
     public const int MaxUsernameLength = 50;
     public const int MaxNameLength = 255;
+    public const int MinPasswordLength = 6;
+    public const int MaxPasswordLength = 64;
+    
+    private static readonly List<string> ReservedUsernames =
+    [
+        "login",
+        "logout",
+        "register",
+        "feed"
+    ];
     
     public static string? ValidateUsername(string username)
     {
@@ -15,6 +25,11 @@ public static class UserValidation
         if (username.Length > MaxUsernameLength)
         {
             return "Username is too long";
+        }
+
+        if (ReservedUsernames.Contains(username))
+        {
+            return "This username is reserved and cannot be used";
         }
 
         return null;
@@ -30,6 +45,26 @@ public static class UserValidation
         if (name.Length > MaxNameLength)
         {
             return "Name is too long";
+        }
+
+        return null;
+    }
+
+    public static string? ValidatePassword(string password)
+    {
+        if (string.IsNullOrWhiteSpace(password))
+        {
+            return "Password cannot be empty";
+        }
+        
+        if (password.Length < MinPasswordLength)
+        {
+            return "Password is too short";
+        }
+
+        if (password.Length > MaxPasswordLength)
+        {
+            return "Password is too long";
         }
 
         return null;
